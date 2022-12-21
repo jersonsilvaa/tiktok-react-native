949c770d26764fb3e8bc7982e1e29c9c229b6354
devsilva <jerson.silva99@hotmail.com>
2022-12-21 14:25:52 -0500

    feat: Add colors theme

---
+++ src/context/DarkTheme.js
@@ -0,0 +1,28 @@
+export const darkTheme = {
+    Default: {
+        border: '#3b3b3b',
+        background: '#121212',
+        backgroundSecondary: '#1f1f1f'
+        
+    },
+    Text: {
+        style: {
+            color: '#ffffff'
+        }
+    },
+    Icon: {
+        iconStyle: {
+            color: '#ffffff'
+        }
+    },
+    Input: {
+        style: {
+            color: '#ffffff'
+        }
+    },
+    Button: {
+        buttonStyle: {
+            backgroundColor: '#2185d0'
+        }
+    }
+}
---
+++ src/context/LightTheme.js
@@ -0,0 +1,28 @@
+export const lightTheme = {
+    Default: {
+        border: '#ffffff',
+        background: '#e3e3e3',
+        backgroundSecondary: '#000000'
+        
+    },
+    Text: {
+        style: {
+            color: '#000000'
+        }
+    },
+    Icon: {
+        iconStyle: {
+            color: '#000000'
+        }
+    },
+    Input: {
+        style: {
+            color: '#000000'
+        }
+    },
+    Button: {
+        buttonStyle: {
+            backgroundColor: '#2185d0'
+        }
+    }
+}
