061a0ba79a0cc7e9da2147dc754d94e4ab0a7296
devsilva <jerson.silva99@hotmail.com>
2022-12-21 17:16:27 -0500

    feat: Change theme onPress button

---
+++ src/navigation/RootNavigation.js
@@ -0,0 +1,20 @@
+import { SafeAreaView } from "react-native-safe-area-context"
+import { Text, Button } from "react-native-elements"
+import { useTheme } from '../utils/import'
+
+const RootNavigation = () => {
+    const { darkMode, toggleTheme } = useTheme()
+    const mode = {
+        theme: darkMode ? 'Cambiar modo claro' : 'Cambiar modo oscuro',
+        mode: darkMode ? 'Modo oscuro' : 'Modo claro'
+    }
+    return <>
+        <SafeAreaView>
+            <Text>Este es la navegación</Text>
+            <Text>Modo {mode.mode}</Text>
+            <Button title={mode.theme} onPress={toggleTheme} />
+        </SafeAreaView>
+    </>
+}
+
+export default RootNavigation
