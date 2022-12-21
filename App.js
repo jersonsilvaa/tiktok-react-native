e5e545397edaed2721df569c48f1475cfa36b7a2
devsilva <jerson.silva99@hotmail.com>
2022-12-21 17:15:44 -0500

    feat: Add theme in app

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { Button, Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-import { ThemeProvider } from './src/utils/import'
+import { ThemeProvider, RootNavigation } from './src/utils/import'
 
 const App = () => {
   return <>
     <ThemeProvider>
-      <SafeAreaView>
-        <Text>Estamos en App</Text>
-        <Button title='Cambiar tema light mode' />
-      </SafeAreaView>
+      <RootNavigation />
     </ThemeProvider>
   </>
 }
