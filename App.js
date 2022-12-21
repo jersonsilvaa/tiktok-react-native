b96dfec3e76fba3beb1925795a4eed8931d3301c
devsilva <jerson.silva99@hotmail.com>
2022-12-21 12:56:52 -0500

    feat: Import and use themeProvider

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, SafeAreaView, StyleSheet } from 'react-native'
-import { Button } from 'react-native-elements'
+import { Text, SafeAreaView } from 'react-native'
+
+import { ThemeProvider } from './src/utils/import'
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Esto es una prueba</Text>
-      <Button title='Solid button' />
-      <StatusBar style='auto' />
-    </SafeAreaView>
-  )
+  return <>
+    <ThemeProvider>
+      <SafeAreaView>
+        <Text>Estamos en App</Text>
+      </SafeAreaView>
+    </ThemeProvider>
+  </>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff'
-  }
-})
 
 export default App
