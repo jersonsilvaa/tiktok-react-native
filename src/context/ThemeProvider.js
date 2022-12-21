04526c69fac50466a2e7d0fc6b2b29418e786a86
devsilva <jerson.silva99@hotmail.com>
2022-12-21 12:56:05 -0500

    feat: Create context of the theme

---
+++ src/context/ThemeProvider.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, createContext } from 'react'
+
+import { ThemeProvider as Theme } from 'react-native-elements'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export const ThemeContext = createContext({
+    darkMode: true,
+    toggleTheme: () => null,
+    theme: {}
+})
+
+export const ThemeProvider = (props) => {
+    const { children } = props
+    const [darkMode, setDarMode] = useState(true)
+
+    const toggleTheme = () => {
+        setDarMode(!darkMode)
+    }
+
+    const data = {
+        theme: null,
+        darkMode,
+        toggleTheme
+    }
+    return (
+        <ThemeContext.Provider value={data}>
+            <Theme>{ children }</Theme>
+        </ThemeContext.Provider>
+    )
+}
