e5f675354045eedd7153ae60e97ab58b6c44ead9
devsilva <jerson.silva99@hotmail.com>
2022-12-21 20:28:55 -0500

    refactor: Remove import custom hook

--- src/utils/import.js
+++ src/utils/import.js
@@ -1,6 +1,4 @@
-import { useTheme } from '../hooks/useTheme'
 import { ThemeProvider } from '../context/ThemeProvider'
+import {RootNavigation} from '../navigation/RootNavigation'
 
-import RootNavigation from '../navigation/RootNavigation'
-
-export { useTheme, ThemeProvider, RootNavigation }
+export { ThemeProvider, RootNavigation }
