2bf87cf2f6d27cd92d173ad12382520592d1e5d1
devsilva <jerson.silva99@hotmail.com>
2022-12-21 21:01:53 -0500

    refactor: Remove change text from theme

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -8,10 +8,7 @@ import { useTheme } from '../hooks/useTheme'
 export const RootNavigation = () => {
     const auth = false
     const { darkMode, toggleTheme } = useTheme()
-    const mode = {
-        theme: darkMode ? 'Cambiar modo claro' : 'Cambiar modo oscuro',
-        mode: darkMode ? 'Modo oscuro' : 'Modo claro'
-    }
+    
     return <>
         <StatusBar animated barStyle={darkMode ? 'dark-content' : 'light-content'} />
         { auth ? <AppNavigation /> : <AuthNavigation /> }
