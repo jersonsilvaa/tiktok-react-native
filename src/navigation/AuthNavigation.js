31194eaef968b03d697740228a50d9a221b75a02
devsilva <jerson.silva99@hotmail.com>
2022-12-21 21:00:42 -0500

    feat: Create authnavigation

---
+++ src/navigation/AuthNavigation.js
@@ -0,0 +1,10 @@
+import { Text } from 'react-native-elements'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export const AuthNavigation = () => {
+    return <>
+        <SafeAreaView>
+            <Text>AuthNavigation</Text>
+        </SafeAreaView>
+    </>
+}
