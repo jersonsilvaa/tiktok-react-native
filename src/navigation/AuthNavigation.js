ce6aa7968b6723df4024b04244fe132c3f8a5abe
devsilva <jerson.silva99@hotmail.com>
2022-12-21 22:42:11 -0500

    feat: Apply theme in screen auth

--- src/navigation/AuthNavigation.js
+++ src/navigation/AuthNavigation.js
@@ -1,10 +1,19 @@
-import { Text } from 'react-native-elements'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { useTheme } from '../hooks/useTheme'
+import { AuthScreen, getNavigationTheme } from '../utils/import'
+
+const Stack = createNativeStackNavigator()
 
 export const AuthNavigation = () => {
+    const { theme } = useTheme()
+    const myTheme = getNavigationTheme(theme)
     return <>
-        <SafeAreaView>
-            <Text>AuthNavigation</Text>
-        </SafeAreaView>
+        <NavigationContainer theme={myTheme}>
+            <Stack.Navigator>
+                <Stack.Screen name='auth' component={AuthScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     </>
 }
