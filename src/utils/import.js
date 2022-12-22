660897204b167cc88acc54dcc63696deabfc1735
devsilva <jerson.silva99@hotmail.com>
2022-12-21 22:42:53 -0500

    feat: Import screen and theme

--- src/utils/import.js
+++ src/utils/import.js
@@ -1,4 +1,6 @@
+import  AuthScreen from '../screens/Auth/AuthScreen'
+import { getNavigationTheme } from './navigation-theme'
 import { ThemeProvider } from '../context/ThemeProvider'
-import {RootNavigation} from '../navigation/RootNavigation'
+import { RootNavigation } from '../navigation/RootNavigation'
 
-export { ThemeProvider, RootNavigation }
+export { ThemeProvider, RootNavigation, AuthScreen, getNavigationTheme }
