ff6281f023f4aff972a037f9355ad60a1f4f2976
devsilva <jerson.silva99@hotmail.com>
2022-12-23 14:48:00 -0500

    feat: Create login form

--- src/screens/Auth/LoginEmailScreen/index.js
+++ src/screens/Auth/LoginEmailScreen/index.js
@@ -1,10 +1,37 @@
-import { Text } from 'react-native-elements'
+import { useState } from 'react'
+import { View } from 'react-native'
+import { Button, Input } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+import { styled } from './styles'
+
 const LoginEmailScreen = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const styles = styled()
+
+  const onShowPasswordInput = () => setShowPassword(prevState => !prevState)
   return (
-    <SafeAreaView>
-      <Text>LoginEmailScreen</Text>
+    <SafeAreaView style={styles.content}>
+      <View style={styles.form_content}>
+                <Input
+                    placeholder='Ingrese su correo electrónico'
+                    autoCapitalize='none'
+                />
+                <Input
+                    placeholder='Ingrese su contraseña'
+                    secureTextEntry={!showPassword}
+                    rightIcon={{
+                        type: 'material-community',
+                        onPress: onShowPasswordInput,
+                        name: showPassword ? 'eye-off-outline' : 'eye-outline'
+                    }}
+                />
+            </View>
+
+            <Button
+                title='Iniciar sesión'
+                containerStyle={styles.button_content}
+            />
     </SafeAreaView>
   )
 }
