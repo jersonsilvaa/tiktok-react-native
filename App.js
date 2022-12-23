dafe15f113ce14b33febd7163d0fb00b151ddf9b
devsilva <jerson.silva99@hotmail.com>
2022-12-23 11:54:08 -0500

    feat: Import util yup

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { Button, Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+import './src/utils/yup-methods'
+
 import { ThemeProvider, RootNavigation } from './src/utils/import'
 
 const App = () => {
