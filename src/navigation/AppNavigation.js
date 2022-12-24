4f3935d6658ed93ae02bbc7baec079e371ceb0c9
devsilva <jerson.silva99@hotmail.com>
2022-12-23 22:56:51 -0500

    feat: Add button

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,10 +1,11 @@
-import { Text } from 'react-native-elements'
+import { Button, Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export const AppNavigation = () => {
     return <>
         <SafeAreaView>
             <Text>AppNavigation</Text>
+            <Button title='Cerrar sesión' />
         </SafeAreaView>
     </>
 }
