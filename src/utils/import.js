243ee47ef512c19ef5b3ffc468b45aa554f94013
devsilva <jerson.silva99@hotmail.com>
2022-12-24 15:49:32 -0500

    feat: Import testscreen

--- src/utils/import.js
+++ src/utils/import.js
@@ -1,5 +1,6 @@
 import { ENV } from './constants'
 import { screen } from './screens'
+import { TestScreen } from '../screens/TestScreen'
 import { ThemeProvider } from '../context/ThemeProvider'
 import { RootNavigation } from '../navigation/RootNavigation'
 import { getNavigationTheme } from './navigation-theme'
@@ -8,4 +9,4 @@ import AuthScreen from '../screens/Auth/AuthScreen'
 import LoginEmailScreen from '../screens/Auth/LoginEmailScreen'
 import RegisterEmailScreen from '../screens/Auth/RegisterEmailScreen'
 
-export { ThemeProvider, RootNavigation, AuthScreen, getNavigationTheme, RegisterEmailScreen, LoginEmailScreen, screen, ENV }
+export { ThemeProvider, RootNavigation, AuthScreen, getNavigationTheme, RegisterEmailScreen, LoginEmailScreen, screen, ENV, TestScreen }
