4feb9cc5af0cf8368155e8ea392ef8d895386ae1
devsilva <jerson.silva99@hotmail.com>
2022-12-24 15:49:11 -0500

    feat: Screen test

---
+++ src/screens/TestScreen.js
@@ -0,0 +1,10 @@
+import { Button, Text } from "react-native-elements"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+export const TestScreen = () => {
+    return <>
+        <SafeAreaView>
+            <Text>TestScreen</Text>
+        </SafeAreaView>
+    </>
+}
