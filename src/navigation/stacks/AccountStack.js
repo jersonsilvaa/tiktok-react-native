dfd2c66d1d9419cd65cd3f4db9f27bfca663d6fc
devsilva <jerson.silva99@hotmail.com>
2022-12-27 15:27:37 -0500

    feat: Create group modal for the options of the account

--- src/navigation/stacks/AccountStack.js
+++ src/navigation/stacks/AccountStack.js
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { AccountScreen, screen } from '../../utils/import'
+import { AccountScreen, ChangeDescription, ChangeInstagramScreen, ChangeNameScreen, ChangeWebSiteScreen, SettingsScreen, screen } from '../../utils/import'
 
 const Stack = createNativeStackNavigator()
 
@@ -8,10 +8,38 @@ export const AccountStack = () => {
     return <>
         <Stack.Navigator>
             <Stack.Screen
-            name={screen.account.account}
-            component={AccountScreen}
-            options={{headerShown: false}}
-        />
+                name={screen.account.account}
+                component={AccountScreen}
+                options={{ title: 'Perfil' }}
+            />
+
+            <Stack.Group screenOptions={{presentation: 'modal'}}>
+                <Stack.Screen
+                    name={screen.account.config}
+                    component={SettingsScreen}
+                    options={{ title: 'Ajustes' }}
+                />
+                <Stack.Screen
+                    name={screen.account.changeName}
+                    component={ChangeNameScreen}
+                    options={{ title: 'Nombre' }}
+                />
+                <Stack.Screen
+                    name={screen.account.changeDescription}
+                    component={ChangeDescription}
+                    options={{ title: 'Descripción corta' }}
+                />
+                <Stack.Screen
+                    name={screen.account.changeWebsite}
+                    component={ChangeWebSiteScreen}
+                    options={{ title: 'Página web' }}
+                />
+                <Stack.Screen
+                    name={screen.account.changeInstagram}
+                    component={ChangeInstagramScreen}
+                    options={{ title: 'Instagram' }}
+                />
+            </Stack.Group>
         </Stack.Navigator>
     </>
 }
