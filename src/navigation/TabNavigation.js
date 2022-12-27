76046385910dc8d1a5ebe0b58dabef107f827338
devsilva <jerson.silva99@hotmail.com>
2022-12-26 19:05:54 -0500

    feat: Import and use title screens

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -1,19 +1,18 @@
-import { Text } from "react-native-elements"
-import { SafeAreaView } from "react-native-safe-area-context"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import { TestScreen } from "../screens/TestScreen"
+import { TestScreen } from '../screens/TestScreen'
+import { screen, HomeStack } from '../utils/import'
 
 const Tab = createBottomTabNavigator()
 
 export const TabNavigation = () => {
     return <>
         <Tab.Navigator>
-            <Tab.Screen name='Inicio' component={TestScreen} />
-            <Tab.Screen name='Amigos' component={TestScreen} />
-            <Tab.Screen name='Subir' component={TestScreen} />
-            <Tab.Screen name='Notificaciones' component={TestScreen} />
-            <Tab.Screen name='Perfil' component={TestScreen} />
+            <Tab.Screen name={screen.home.tab} component={HomeStack} />
+            <Tab.Screen name={screen.friends.tab} component={TestScreen} />
+            <Tab.Screen name={screen.upload.tab} component={TestScreen} />
+            <Tab.Screen name={screen.notifications.tab} component={TestScreen} />
+            <Tab.Screen name={screen.account.tab} component={TestScreen} />
         </Tab.Navigator>
     </>
 }
