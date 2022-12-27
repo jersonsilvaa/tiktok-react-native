574f8b35d8260de90477e77defdf0c5da57d9b88
devsilva <jerson.silva99@hotmail.com>
2022-12-27 13:12:21 -0500

    feat: Using creens un the tabnav

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -1,18 +1,37 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import { TestScreen } from '../screens/TestScreen'
-import { screen, HomeStack } from '../utils/import'
+import { screen, HomeStack, FriendsStack, UploadVideo, NotificationsScreen, AccountScreen } from '../utils/import'
 
 const Tab = createBottomTabNavigator()
 
 export const TabNavigation = () => {
     return <>
         <Tab.Navigator>
-            <Tab.Screen name={screen.home.tab} component={HomeStack} />
-            <Tab.Screen name={screen.friends.tab} component={TestScreen} />
-            <Tab.Screen name={screen.upload.tab} component={TestScreen} />
-            <Tab.Screen name={screen.notifications.tab} component={TestScreen} />
-            <Tab.Screen name={screen.account.tab} component={TestScreen} />
+            <Tab.Screen
+                name={screen.home.tab}
+                component={HomeStack}
+                options={{headerShown: false}}
+            />
+            <Tab.Screen
+                name={screen.friends.tab}
+                component={FriendsStack}
+                options={{headerShown: false}}
+            />
+            <Tab.Screen
+                name={screen.upload.tab}
+                component={UploadVideo}
+                options={{headerShown: false}}
+            />
+            <Tab.Screen
+                name={screen.notifications.tab}
+                component={NotificationsScreen}
+                options={{headerShown: false}}
+            />
+            <Tab.Screen
+                name={screen.account.tab}
+                component={AccountScreen}
+                options={{headerShown: false}}
+            />
         </Tab.Navigator>
     </>
 }
