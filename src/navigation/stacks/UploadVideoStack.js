4f2826b709c7e7d117fcf5ecd5b56a149517da84
devsilva <jerson.silva99@hotmail.com>
2022-12-27 15:32:01 -0500

    feat: Add screen for publish video

--- src/navigation/stacks/UploadVideoStack.js
+++ src/navigation/stacks/UploadVideoStack.js
@@ -1,15 +1,23 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { UploadVideo, screen } from '../../utils/import'
+import { screen, UploadVideo, PublishVideoScreen } from '../../utils/import'
 
 const Stack = createNativeStackNavigator()
 
 export const UploadVideoStack = () => {
-    <Stack.Navigator>
-        <Stack.Screen
-            name={screen.upload.uploadVideo}
-            component={UploadVideo}
-            options={{headerShown: false}}
+    return <>
+        <Stack.Navigator>
+            <Stack.Screen
+                name={screen.upload.uploadVideo}
+                component={UploadVideo}
+                options={{title: ''}}
+            />
+
+            <Stack.Screen
+                name={screen.upload.publishVideo}
+                component={PublishVideoScreen}
+                options={{title: 'Publiar'}}
         />
-    </Stack.Navigator>
+        </Stack.Navigator>
+    </>
 }
