ddf756164b3d58d47c52a3314d68a4cabbe0530a
devsilva <jerson.silva99@hotmail.com>
2022-12-28 15:35:14 -0500

    feat: Create field for the video

--- video/models.py
+++ video/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import os
 
-# Create your models here.
+def getVideoPath(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = '%s.%s' % (instance.id, ext)
+
+    return os.path.join(f'video/{filename}')
+
+def getImagePath(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = '%s.%s' % (instance.id, ext)
+
+    return os.path.join(f'video_image/{filename}')
+
+class Video(models.Model):
+    description = models.TextField()
+    video = models.FileField(upload_to=getVideoPath)
+    image = models.ImageField(upload_to=getImagePath)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    sharedCounter = models.IntegerField(default=0)
+    likesCounter = models.IntegerField(default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
