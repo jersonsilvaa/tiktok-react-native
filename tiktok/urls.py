9bfc2d26a901bef79b3129e4ed728f62b63969ba
devsilva <jerson.silva99@hotmail.com>
2022-12-28 17:32:43 -0500

    feat: Include path for the video

--- tiktok/urls.py
+++ tiktok/urls.py
@@ -10,8 +10,8 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the -() function: from django.urls import -, path
+    2. Add a URL to urlpatterns:  path('blog/', -('blog.urls'))
 """
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,6 +21,8 @@ from django.urls import path, include
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+
+from video.api.router import routerVideo
 
 
 schema_view = get_schema_view(
@@ -40,5 +42,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.api.router')),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include(routerVideo.urls)),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
