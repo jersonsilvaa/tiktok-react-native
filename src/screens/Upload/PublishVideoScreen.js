2e1ee7250657f7c22d74f0f9b395dd924cf34233
devsilva <jerson.silva99@hotmail.com>
2022-12-29 15:51:20 -0500

    feat: Get dates for the video uploaded

--- src/screens/Upload/PublishVideoScreen.js
+++ src/screens/Upload/PublishVideoScreen.js
@@ -1,7 +1,8 @@
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-export const PublishVideoScreen = () => {
+export const PublishVideoScreen = (props) => {
+  console.log(props)
   return <>
     <SafeAreaView>
       <Text>Publicar video</Text>
